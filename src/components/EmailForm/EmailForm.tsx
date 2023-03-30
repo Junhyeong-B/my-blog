2ab05179b56af64ef58d9bcd41de2af30cef912c
Junhyeong-B <aowlr0513@gmail.com>
2023-03-30 15:21:07 +0900

    feat: modify to send email in contact page

--- src/components/EmailForm/EmailForm.tsx
+++ src/components/EmailForm/EmailForm.tsx
@@ -4,7 +4,8 @@ import useForm from '@/hooks/useForm';
 import { useState } from 'react';
 import { useRef } from 'react';
 import { useEffect } from 'react';
-import Alert from '../Alert/Alert';
+import Alert from '@/components/Alert';
+import { sendContactEmail } from '@/service/contact';
 
 export default function EmailForm() {
   const formRef = useRef<HTMLFormElement>(null);
@@ -12,35 +13,36 @@ export default function EmailForm() {
   const [showSuccessAlert, setShowSuccessAlert] = useState(false);
   const { values, error, handleChange, handleSubmit, reset } = useForm({
     initialValue: {
-      email: '',
+      from: '',
       subject: '',
       message: '',
     },
     onSubmit: async (value) => {
-      console.log(value);
+      await sendContactEmail(value);
       formRef.current?.reset();
       reset();
     },
-    validate: ({ email, subject, message }) => {
+    validate: ({ from, subject, message }) => {
       const errors: Record<string, string> = {};
 
-      if (!email) errors.email = '이메일을 입력해주세요.';
+      if (!from) errors.from = '이메일을 입력해주세요.';
       if (!subject) errors.subject = '제목을 입력해주세요.';
       if (!message) errors.message = '이메일 본문을 입력해주세요.';
-      if (!errors.email && !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email))
-        errors.email = '올바른 이메일을 입력해주세요.';
+      if (!errors.from && !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(from))
+        errors.from = '올바른 이메일을 입력해주세요.';
 
       return errors;
     },
-    beforeSubmit: ({ error }) => {
-      if (Object.keys(error).length > 0) {
-        setShowFailAlert(true);
-      } else {
-        setShowSuccessAlert(true);
-      }
+    onSuccess: () => {
+      setShowSuccessAlert(true);
+      setTimeout(() => {
+        setShowSuccessAlert(false);
+      }, 2000);
+    },
+    onFail: () => {
+      setShowFailAlert(true);
       setTimeout(() => {
         setShowFailAlert(false);
-        setShowSuccessAlert(false);
       }, 2000);
     },
   });
@@ -61,15 +63,15 @@ export default function EmailForm() {
           className="flex flex-col gap-1"
           ref={formRef}
         >
-          <label className="font-semibold text-white" htmlFor="email">
+          <label className="font-semibold text-white" htmlFor="from">
             Your Email
           </label>
           <input
             className="rounded-sm p-1"
             onChange={handleChange}
-            id="email"
-            type="text"
-            name="email"
+            id="from"
+            type="email"
+            name="from"
           />
           <label className="font-semibold text-white" htmlFor="subject">
             Subject
