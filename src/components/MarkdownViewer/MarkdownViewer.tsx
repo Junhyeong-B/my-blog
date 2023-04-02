ba0dbe6aa9d8471c963a9488683ff0291135e1a4
Junhyeong-B <aowlr0513@gmail.com>
2023-04-03 00:18:32 +0900

    feat: add MarkdownViewer component

---
+++ src/components/MarkdownViewer/MarkdownViewer.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import ReactMarkDown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+type Props = {
+  content: string;
+};
+
+export default function MarkdownViewer({ content }: Props) {
+  return (
+    <ReactMarkDown
+      className="prose lg:prose-xl"
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '');
+          return !inline && match ? (
+            <SyntaxHighlighter
+              language={match[1]}
+              PreTag="div"
+              {...props}
+              style={coldarkDark}
+            >
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
+      {content}
+    </ReactMarkDown>
+  );
+}
