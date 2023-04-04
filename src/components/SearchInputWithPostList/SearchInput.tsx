e064beb7474936411d5a0ecfdd850026e58effc4
Junhyeong-B <aowlr0513@gmail.com>
2023-04-05 00:06:53 +0900

    feat: modify some css and useEffect

--- src/components/SearchInputWithPostList/SearchInput.tsx
+++ src/components/SearchInputWithPostList/SearchInput.tsx
@@ -7,6 +7,7 @@ import debounce from '@/utils/debounce';
 import { AiOutlineSearch } from 'react-icons/ai';
 import SearchResultPostList from './SearchResultPostList';
 import { useRouter } from 'next/navigation';
+import useOnMount from '@/hooks/useOnMount';
 
 type Props = {
   files: FrontMatter[];
@@ -16,15 +17,14 @@ export default function SearchInput({ files }: Props) {
   const router = useRouter();
 
   const postsRef = useRef(files);
+  const inputRef = useRef<HTMLInputElement>(null);
   const [searchText, setSearchText] = useState('');
   const [searchedPosts, setSearchedPosts] = useState<FrontMatter[]>([]);
   const [resultPosts, setResultPosts] = useState<FrontMatter[]>([]);
 
-  const showResult = postsRef.current.length !== searchedPosts.length;
-
-  useEffect(() => {
+  useOnMount(() => {
     setResultPosts(files);
-  }, [files]);
+  });
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = debounce(
     (e) => {
@@ -49,6 +49,7 @@ export default function SearchInput({ files }: Props) {
     e.preventDefault();
 
     if (searchedPosts.length === 0) {
+      setResultPosts(postsRef.current);
       return;
     }
 
@@ -61,6 +62,10 @@ export default function SearchInput({ files }: Props) {
       setResultPosts(searchedPosts);
       setSearchedPosts([]);
       setSearchText('');
+
+      if (inputRef.current) {
+        inputRef.current.value = '';
+      }
     }
   };
 
@@ -72,11 +77,12 @@ export default function SearchInput({ files }: Props) {
           className="box-border w-full rounded-full border-2 border-slate-300 py-2 pr-16 pl-4 outline-none"
           placeholder="검색어를 입력해주세요."
           onChange={handleChange}
+          ref={inputRef}
         />
         <button className="absolute right-0 rounded-full border-2 border-l-0 border-slate-300 bg-slate-800 py-2 px-4 text-slate-50 transition-colors hover:bg-slate-600">
           <AiOutlineSearch size="24px" />
         </button>
-        {showResult && searchedPosts.length > 0 && (
+        {searchedPosts.length > 0 && (
           <SearchResultPostList posts={searchedPosts} searchText={searchText} />
         )}
       </form>
--- src/components/SearchInputWithPostList/SearchResultPostList.tsx
+++ src/components/SearchInputWithPostList/SearchResultPostList.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function SearchResultPostList({ posts }: Props) {
   return (
-    <ul className="animate-fadeIn absolute top-14 max-h-40 w-full overflow-y-auto border border-slate-600 bg-slate-100 ">
+    <ul className="animate-fadeIn absolute top-14 max-h-40 w-full overflow-y-auto border border-slate-600 bg-slate-100">
       {posts.map((post) => (
         <li className="cursor-pointer p-2 hover:bg-slate-300" key={post.title}>
           <Link href={`/blog/${post.slug}`}>{post.title}</Link>
